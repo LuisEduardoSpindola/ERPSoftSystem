e9466033fdd800a076914c97c280e2f8308b539d
Luis Eduardo Spindola <eduspindola@live.com>
2023-12-19 23:10:07 -0300

    Azure

--- ERPSoft.DATA/Models/ERPSoftDbContext.cs
+++ ERPSoft.DATA/Models/ERPSoftDbContext.cs
@@ -38,7 +38,7 @@ public partial class ERPSoftDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=LUIS_EDUARDO;Initial Catalog=ERPSoft;Persist Security Info=True;User ID=sa;Password=senha;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Data Source=LUIS-EDUARDO\\SQLMAIN;Initial Catalog=ERPSoft;User ID=sa;Password=senha;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- ERPSoft.Web/Program.cs
+++ ERPSoft.Web/Program.cs
@@ -52,11 +52,10 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-app.MapRazorPages();
-
 app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
